97bfe23064d6d128eb99d79d2d7a7a8ced14f96a
Miny <944307891@qq.com>
2021-08-20 22:44:29 +0800

    发布动态-插入动态数据到数据库

--- src/controller/moment.controller.js
+++ src/controller/moment.controller.js
@@ -1,7 +1,16 @@
+const momentService = require("../service/moment.service");
+
 class MomentController {
-    async create(ctx, next) {
-        ctx.body = "发表动态成功";
-    }
+  async create(ctx, next) {
+    // 1. 获取数据(user_id, content )
+    // 授权成功时 ctx.user 包含 id
+    const userId = ctx.user.id;
+    const content = ctx.request.body.content;
+
+    // 2.将数据插入到数据库
+    const result = await momentService.create(userId, content);
+    ctx.body = result;
+  }
 }
 
 module.exports = new MomentController();
--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -1,9 +1,12 @@
-const connection = require('../app/database');
+const connection = require("../app/database");
 
 class MomentService {
-    async create() {
-
-    }
+  async create(userId, content) {
+    const statement = `INSERT INTO moment (user_id, content) VALUES (?, ?);`;
+    const result = await connection.execute(statement, [userId, content]);
+    // 返回数据
+    return result[0];
+  }
 }
 
 module.exports = new MomentService();
