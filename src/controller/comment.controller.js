fb5441bc2f0c3e676c34db34290571f1113ddd57
Miny <944307891@qq.com>
2021-08-30 23:17:43 +0800

    回复评论-回复评论的逻辑

--- src/controller/comment.controller.js
+++ src/controller/comment.controller.js
@@ -7,6 +7,14 @@ class CommentController {
     const result = await service.create(momentId, content, id);
     ctx.body = result;
   }
+
+  async reply(ctx, next) {
+    const { momentId, content } = ctx.request.body;
+    const { commentId } = ctx.params;
+    const { id } = ctx.user;
+    const result = await service.reply(momentId, content, commentId, id);
+    ctx.body = result;
+  }
 }
 
 module.exports = new CommentController();
--- src/service/comment.service.js
+++ src/service/comment.service.js
@@ -6,7 +6,13 @@ class CommentService {
     const [result] = await connection.execute(statement, [content, momentId, userId]);
     // 返回数据
     return result;
-    console.log("111");
+  }
+
+  async reply(momentId, content, commentId, userId) {
+    const statement = `INSERT INTO comment (content, moment_id, comment_id, user_id) VALUES (?, ?, ?, ?);`;
+    const [result] = await connection.execute(statement, [content, momentId, commentId, userId]);
+    // 返回数据
+    return result;
   }
 }
 
