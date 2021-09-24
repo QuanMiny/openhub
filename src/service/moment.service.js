041d929aa5270c5ec0a3f5008faba6050f80db40
Miny <944307891@qq.com>
2021-09-24 22:40:14 +0800

    查询动态的标签和评论SQL修改

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -18,19 +18,23 @@ class MomentService {
 
   async getMomentById(id) {
     const statement = `
-        SELECT 
-        m.id id, m.content content, m.createAt createTime, m.updateAt updateTime,
-        JSON_OBJECT('id', u.id, 'name', u.name) author,
-        JSON_ARRAYAGG(
-          JSON_OBJECT('id', c.id, 'content', c.content, 'createTime', c.createAt,
-                      'user', JSON_OBJECT('id', cu.id, 'name', cu.name))
-        ) comments
-        FROM moment m
-        LEFT JOIN user u ON m.user_id = u.id
-        LEFT JOIN comment c ON c.moment_id = m.id
-        LEFT JOIN user cu ON c.user_id = cu.id
-        WHERE m.id = ?;
-      `;
+    SELECT 
+      m.id id, m.content content, m.createAt createTime, m.updateAt updateTime,
+      JSON_OBJECT('id', u.id, 'name', u.name) author,
+      IF(COUNT(l.id), JSON_ARRAYAGG(
+        JSON_OBJECT('id', l.id, 'name', l.name)
+      ), NULL) labels,
+      (SELECT IF(COUNT(c.id), JSON_ARRAYAGG(
+        JSON_OBJECT('id', c.id, 'content', c.content, 'createTime', c.createAt,
+                    'user', JSON_OBJECT('id', cu.id, 'name', cu.name))
+      ), NULL) FROM comment c LEFT JOIN user cu ON c.user_id = cu.id WHERE m.id = c.moment_id) comments
+    FROM moment m
+    LEFT JOIN user u ON m.user_id = u.id
+    LEFT JOIN moment_label ml ON m.id = ml.moment_id
+    LEFT JOIN label l ON ml.label_id = l.id
+    WHERE m.id = ?
+    GROUP BY m.id;
+    `;
     const [result] = await connection.execute(statement, [id]);
     return result[0];
   }
@@ -40,7 +44,8 @@ class MomentService {
         SELECT 
           m.id id, m.content content, m.createAt createTime, m.updateAt updateTime,
           JSON_OBJECT('id', u.id, 'name', u.name) author,
-          (SELECT COUNT(*) FROM comment c WHERE c.moment_id = m.id) commentCount
+          (SELECT COUNT(*) FROM comment c WHERE c.moment_id = m.id) commentCount,
+          (SELECT COUNT(*) FROM moment_label ml WHERE ml.moment_id = m.id) labelCount
         FROM moment m
         LEFT JOIN user u ON m.user_id = u.id
         LIMIT ?, ?;
